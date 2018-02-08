459109194e4275c85dca7f0e2754037ec0eac2a9
jarrm5 <rinellaj2448@gmail.com>
2018-02-07 20:54:58 -0600

    handle topics and news sources at the same time

--- NewsApp/js/home.js
+++ NewsApp/js/home.js
@@ -1,20 +1,44 @@
 
+var URL = 'https://newsapi.org/v2/everything?' +
+          'language=en&' +
+          'from=2018-01-01&' +
+          'sortBy=relevancy&' +
+          'apiKey=4e7410f126044cd0807b3f536176ea48&';
+
 $(document).ready(function(){
 
     $('#news-sources').html(populate_news_outlets());
 
     $('#btn-search').on('click',function(){
+
         var topic = $('#inp-topic').val().trim();
 
+        var selected = [];
+        $.each($('input:checkbox:checked'),function(){
+            selected.push($(this).val());
+        });
+
+        
         if(topic !== ""){
-            get_articles(get_topic_url(topic));
+            URL += ('q=' + topic);
         }
 
+        if(selected.length != 0){
+            if(topic !== ""){
+                URL += "&";
+            }
+            URL += ('sources=' + get_source_str(selected));
+        }    
+        console.log(Math.floor(20/3));
+        get_articles(URL);
     });
 
     $('#btn-reset').on('click', function(){
         $('#inp-topic').val('');
-        
+
+        $.each($('input:checkbox:checked'),function(){
+            $(this).attr('checked',false);
+        });  
     });
 });
 
@@ -33,24 +57,6 @@ function get_articles(theURL){
     });
 }
 
-
-function get_topic_url(topic){
-    return  'https://newsapi.org/v2/everything?' +
-            'language=en&' +
-            'q=' + topic + '&' +
-            'from=2018-01-01&' +
-            'sortBy=relevancy&' +
-            'apiKey=4e7410f126044cd0807b3f536176ea48';
-}
-function get_source_url(sources){
-    return  'https://newsapi.org/v2/everything?' +
-            'language=en&' +
-            'domains=' + sources + '&' +
-            'from=2018-01-01&' +
-            'sortBy=relevancy&' +
-            'apiKey=4e7410f126044cd0807b3f536176ea48';
-}
-
 function populate_news_outlets(){
     var html = '';
     var outlets = get_news_outlets();
@@ -65,6 +71,17 @@ function populate_news_outlets(){
                 '</div>';
     }
     return html;
+}
+
+function get_source_str(sourceList){
+    var sourceStr = '';
+    for(var i = 0; i < sourceList.length; i++){
+        sourceStr += sourceList[i];
+        if(i < sourceList.length - 1){
+            sourceStr += ',';
+        }
+    }
+    return sourceStr;
 }
 
 function NewsOutlet(id,name,img){
@@ -85,8 +102,8 @@ function get_news_outlets(){
         new NewsOutlet('msnbc','MSNBC','msnbc.PNG'),
         new NewsOutlet('the-new-york-times','The New York Times','nyt.PNG'),
         new NewsOutlet('usa-today','USA Today','usa.PNG'),      
-        new NewsOutlet('the-huffington-post','The Huffington Post','huff.PNG'),
-        new NewsOutlet('the-wall-street-journal','the Wall Street Journal','wsj.PNG')      
+        new NewsOutlet('the-huffington-post','The Huffington Post','Huffpost.jpg'),
+        new NewsOutlet('the-wall-street-journal','The Wall Street Journal','wsj.PNG')      
     ];
 }
 
